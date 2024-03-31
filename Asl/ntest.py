0e54fd2d51cbde36c3b09468e2e8f4595ce44931
Bhagyashri V Zade <125757446+Code-Eagl@users.noreply.github.com>
2024-03-31 19:00:14 +0530

    Add files via upload

---
+++ Asl/ntest.py
@@ -0,0 +1,59 @@
+import numpy as np
+import tensorflow
+import math
+import time
+import cv2
+import cvzone
+from cvzone.ClassificationModule import Classifier
+from cvzone.HandTrackingModule import HandDetector
+cap =cv2.VideoCapture(0)
+detector =HandDetector(maxHands=2)
+classifier= Classifier("model2/keras_model.h5","model2/labels.txt")
+offset = 20
+imgSize=300
+counter = 0
+labels = ["0","1","2","3","4","5","6","7","9","10","8"]
+while True:
+    success, img =cap.read()
+    imgout=img.copy()
+    hands, img= detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h=hand['bbox']
+        imgWhite =np.ones((imgSize,imgSize,3),np.uint8)*255
+        imgCrop=img[y - offset:y+h+offset, x-offset:x+w+offset]
+        imgCropShape = imgCrop.shape
+
+        aspectRatio = h/w
+        if aspectRatio >1:
+            k = imgSize/h
+            wCal= math.ceil(k*w)
+            imgResize =cv2.resize(imgCrop,(wCal,imgSize))
+            imgResizeShape = imgResize.shape
+            wgap = math.ceil((imgSize-wCal)/2)
+            imgWhite[:, wgap:wCal+wgap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
+        else:
+            k = imgSize/w
+            hCal= math.ceil(k*h)
+            imgResize =cv2.resize(imgCrop,(imgSize,hCal))
+            imgResizeShape = imgResize.shape
+            hgap = math.ceil((imgSize-hCal)/2)
+            imgWhite[hgap:hCal+hgap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+        cv2.rectangle(imgout, (x - offset, y-offset-50),
+                     (x - offset+90, y-offset-50+50), (255,0,255),cv2.FILLED)
+        cv2.putText(imgout,labels[index],(x,y-25),cv2.FONT_HERSHEY_COMPLEX,1.7,(255,655,255),2)
+        cv2.rectangle(imgout,(x-offset, y-offset),
+                      (x+w+offset,y+h+offset),(255,0,255),4)
+
+        cv2.imshow("ImageCrop",imgCrop)
+        cv2.imshow("ImageWhite",imgWhite)
+    cv2.imshow("Image",imgout)
+    cv2.waitKey(1)
+
+
+
